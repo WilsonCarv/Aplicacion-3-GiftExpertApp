9699c5ce5f6a7af120e02d314c1c7114448f45e8
WilCarv <alfaro9331@gmail.com>
2020-08-28 19:20:37 -0600

    docs agregado:

---
+++ docs/precache-manifest.b152e6b4f00f253216d50a6067e1432e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fb2b7df586b107417ebbfd3bcaa7ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0313af131246a3edd2d1",
+    "url": "/static/css/main.3f4b8bd6.chunk.css"
+  },
+  {
+    "revision": "f282757b0c05702f74d8",
+    "url": "/static/js/2.b1f59dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1f59dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0313af131246a3edd2d1",
+    "url": "/static/js/main.24588e49.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
